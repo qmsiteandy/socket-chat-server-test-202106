6493c9bed5861975aeb27535a283946388f5939b
qmsiteandy <ht93084@gmail.com>
2021-06-22 14:19:04 +0800

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const port = process.env.PORT || 5500;
+
+//匯入socket
 const socket = require("socket.io");
 
 const app = express();
@@ -11,17 +13,20 @@ server.listen(port, () => {
   console.log("Socket IO server listening at port %d", port);
 });
 
+//socket監聽server
 const io = socket(server);
 
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
+//當新的client建立連線
 io.on("connection", function (socket) {
-  console.log("connection", socket.id);
 
+  // 接收client傳來的資料
   socket.on("sendMessage", function (data) {
     console.log(data);
+    // 傳送資料到所有client
     io.emit("newMessage", data);
   });
 });
